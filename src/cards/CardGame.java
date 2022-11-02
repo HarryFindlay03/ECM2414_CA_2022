928599ae62d80e76c906a855839f8c500f957088
Harry Findlay <harryfindlay@outlook.com>
2022-11-02 17:27:28 +0000

    Tests for gameSetup

--- src/cards/CardGame.java
+++ src/cards/CardGame.java
@@ -5,8 +5,39 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class CardGame {
+    private int numPlayers;
+    private int deckLength;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("hello, world!");
+    }
+
+    public CardGame(int numPlayers, String filename) throws InvalidPackException{
+        if(numPlayers <= 0) {
+            throw new InvalidPackException("Number of players needs to be greater than 0");
+        }
+        this.numPlayers = numPlayers;
+        this.deckLength = ((8 * numPlayers) - (4 * numPlayers)) / 4;
+    }
+
+    //setup game
+    public void gameSetup() {
+        for(int i = 0; i < numPlayers; i++) {
+            createPlayer();
+            createDeck();
+        }
+    }
+
+    //create players
+    public int createPlayer() {
+        Player player = new Player();
+        return player.getPlayerId();
+    }
+
+    //create decks
+    public int createDeck() {
+        Deck deck = new Deck(deckLength);
+        return deck.getDeckId();
     }
 }
--- tests/testCardGame.java
+++ tests/testCardGame.java
@@ -2,20 +2,45 @@ import cards.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class testCardGame {
-    ImplementCardGameInterface cgInterface = new ImplementCardGameInterface(4, "test.txt");
+    CardGame cardGame = new CardGame(4, "test.txt");
 
-    @Test
-    void testCreatePlayer() {
-        assertEquals(1, cgInterface.createPlayer());
-    }
-    @Test
-    void testCreatePack() throws InvalidPackException{
-        //for pack size 4
-        assertEquals(32, cgInterface.createPack());
+    public testCardGame() throws InvalidPackException {
     }
 
+    @BeforeEach
+    void setUp() {
+        cardGame.gameSetup();
+    }
+
+    @AfterEach
+    void cleanUp() {
+        Player.getPlayerIds().clear();
+        Deck.getDeckIds().clear();
+    }
+
+    @Test
+    void testPlayers() {
+        assertEquals(4, Player.getPlayerIds().size());
+    }
+
+    @Test
+    void testDecks() {
+        assertEquals(4, Deck.getDeckIds().size());
+    }
+
+    @Test
+    void testInvalidInput() {
+        assertThrows(InvalidPackException.class, () -> {
+           CardGame invalidCardGame = new CardGame(-1, "test.txt");
+        });
+    }
+
+//    @Test
+//    void testPackLength() {
+//        int expectedPackLength = Player.getPlayerIds().size() * 8;
+//        assertEquals(expectedPackLength, );
+//    }
 }
