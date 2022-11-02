de5cacaa06adc8f9422c96db52bf3a044ae957f2
Harry Findlay <harryfindlay@outlook.com>
2022-11-02 22:07:25 +0000

    adding file tests

--- tests/testCardGame.java
+++ tests/testCardGame.java
@@ -6,12 +6,14 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.FileNotFoundException;
+
 public class testCardGame {
     CardGame cardGame;
 
     @BeforeEach
-    void setUp() throws InvalidPackException {
-        cardGame = new CardGame(4, "test.txt");
+    void setUp() throws InvalidPackException, FileNotFoundException {
+        cardGame = new CardGame(2, "packs/2.txt");
         cardGame.gameSetup();
     }
 
@@ -22,15 +24,16 @@ public class testCardGame {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {-1, 0, 1, 2, 4, 5, 50, -999, 999})
-    void testGameSetup(int numPlayers) throws InvalidPackException {
+    @ValueSource(ints = {-1, 0, 1, 2, 4, 5, 50, -999})
+    void testGameSetup(int numPlayers) throws InvalidPackException, FileNotFoundException {
         cleanUp();
+        String filename = "packs/" + String.valueOf(numPlayers) + ".txt";
         if(numPlayers <= 0) {
             assertThrows(InvalidPackException.class, () -> {
-                CardGame invalidCardGame = new CardGame(-1, "test.txt");
+                CardGame invalidCardGame = new CardGame(numPlayers, filename);
             });
         } else {
-            CardGame cg = new CardGame(numPlayers, "test.txt");
+            CardGame cg = new CardGame(numPlayers, filename);
             cg.gameSetup();
             assertEquals(numPlayers, Player.getPlayerIds().size());
             assertEquals(numPlayers, Deck.getDeckIds().size());
@@ -39,12 +42,12 @@ public class testCardGame {
 
     @Test
     void testPlayers() {
-        assertEquals(4, Player.getPlayerIds().size());
+        assertEquals(2, Player.getPlayerIds().size());
     }
 
     @Test
     void testDecks() {
-        assertEquals(4, Deck.getDeckIds().size());
+        assertEquals(2, Deck.getDeckIds().size());
     }
 
     @Test
@@ -54,9 +57,33 @@ public class testCardGame {
         });
     }
 
-//    @Test
-//    void testPackLength() {
-//        int expectedPackLength = Player.getPlayerIds().size() * 8;
-//        assertEquals(expectedPackLength, cardGame.getPackLength());
-//    }
+    @Test
+    void testPackLength() {
+        int expectedPackLength = Player.getPlayerIds().size() * 8;
+        assertEquals(expectedPackLength, cardGame.getPack().size());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-99, 10, 5, 4})
+    void testPack(int numPlayers) throws InvalidPackException, FileNotFoundException{
+        cleanUp();
+        if(numPlayers <= 0) {
+            assertThrows(InvalidPackException.class, () -> {
+                CardGame invalidCardGame = new CardGame(numPlayers, "test.txt");
+            });
+        } else {
+            String filename = "packs/" + String.valueOf(numPlayers) + ".txt";
+            CardGame cg = new CardGame(numPlayers, filename);
+            cg.gameSetup();
+            int expectedPackLength = Player.getPlayerIds().size() * 8;
+            assertEquals(expectedPackLength, cg.getPack().size());
+        }
+    }
+
+    @Test
+    void testFileNotFound() {
+        assertThrows(FileNotFoundException.class, () -> {
+            CardGame cg = new CardGame(4, "fff.txt");
+        });
+    }
 }
